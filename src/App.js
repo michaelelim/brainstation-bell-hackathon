c93aaf94cd86f20f3aa9ef7dd9a37aff67e840cb
Michael Lim <michaelelim@gmail.com>
2020-09-30 22:06:48 -0400

    Breakpoint Adjustments

--- src/App.js
+++ src/App.js
@@ -27,8 +27,11 @@ function App() {
           <img src={Bell} className="navbar__logo" alt=""/>
           <h1 className="navbar__header"><a href="./index.html" className="navbar__link">Screentime</a></h1>
         </div>
-        <div className="navbar__nav-links--active">The Facts</div>
-        <div className="navbar__nav-links"><a href="https://letstalk.bell.ca/en/get-help" className="navbar__nav-links">Get Help</a></div>
+        <div className="navbar__wrapper">
+          <div className="links-active">The Facts</div>
+          {/* <div className="links"><a href="https://letstalk.bell.ca/en/get-help" className="links2">Get Help</a></div> */}
+          <a href="https://letstalk.bell.ca/en/get-help" className="links">Get Help</a>
+        </div>
       </nav>
       <div className="hero">
       <div className="hero__image">
